2701fffee52cfdd7f452d5e0113656b0fc25ce12
pranayh24 <pranayheda24@gmail.com>
2025-12-07 14:19:01 +0530

    Initialized the project

---
+++ src/main/java/pr/nebulastream/NebulaStreamApplication.java
@@ -0,0 +1,13 @@
+package pr.nebulastream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulaStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NebulaStreamApplication.class, args);
+    }
+
+}
